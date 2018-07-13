f50b6a35da9509952ec58e1d8cafb77ada0cf593
Kucharska, Agata <agata.kucharska@sap.com>
2018-07-13 08:46:02 +0200

    make contacts-app-ui  use graphql service

--- contacts-app-ui/src/components/MainPage/MainPage.component.js
+++ contacts-app-ui/src/components/MainPage/MainPage.component.js
@@ -1,18 +1,7 @@
 import React from "react";
-import styled from 'styled-components';
-import { CardStack, Card } from 'react-cardstack';
-// import {
-//   Dimensions,
-//   Scrolldiv,
-//   StyleSheet,
-//   span,
-//   div
-// } from "react-native";
-// import { getStatusBarHeight } from "react-native-status-bar-height";
-// import { CardStack, Card } from "react-native-cardstack";
-// import { Font } from "expo";
-// const { width, height } = Dimensions.get("window");
-// const statusBarHeight = getStatusBarHeight();
+import styled from "styled-components";
+import { CardStack, Card } from "react-cardstack";
+
 const colors = [
   "#00b6ff",
   "#9b72ec",
@@ -23,6 +12,7 @@ const colors = [
   "#0b74de",
   "#ff6000"
 ];
+let rowHeight = 60;
 
 let contacts = null;
 let contactsLength = 0;
@@ -33,220 +23,255 @@ const Center = styled.div`
 `;
 
 const CardStack1 = styled.div`
-box-sizing: border-box;
-width:400px;
-margin: 0 auto;
+  box-sizing: border-box;
+  width: 400px;
+  margin: 0 auto;
 `;
+
 const Title = styled.h1`
-color:white;
-  padding: 0 20px;
-  font-family: '72';
-  font-size:23px;
-  font-weight:100;
+  color: #32363a;
+  font-family: "72";
+  font-size: 23px;
+  font-weight: 400;
+  padding-left: 10px;
+  font-size: 30px;
+  line-height: 30px;
+`;
+const TitleContainer = styled.div`
+  height: 30px;
+  width: 100%;
+  margin-bottom: 10px;
+  background-color: #f3f4f5;
 `;
 
-// const ContactCard = props => {
+const LogoContainer = styled.div`
+  height: ${rowHeight - 20}px;
+  width: ${rowHeight - 20}px;
+  background-color: ${props => colors[props.id % colors.length]};
+  margin-right: 10px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Logo = styled.span`
+  font-size: 20px;
+  color: #fff;
+  font-family: "72";
+`;
+const CardHeader = styled.div`
+  box-sizing: border-box;
+  height: ${rowHeight - 20}px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding-bottom: 10;
+`;
+const RowContainer = styled.div`
+  padding: 10px;
+`;
 
-  
-//   return (
-//     <div>
-//       <div style={[styles.cardHeader, { height: 60 }]}>
-//         <div
-//           style={[
-//             styles.logoContainer,
-//             {
-//               height: 50,
-//               width: 50,
-//               backgroundColor: colors[props.id % colors.length],
-//               marginRight: 20,
-//             }
-//           ]}
-//         >
-//           <span style={[styles.logo, { fontSize: 24, }]}>
-//             {props.firstName.charAt(0)}
-//             {props.lastName.charAt(0)}
-//           </span>
-//         </div>
-//         <span style={[{ fontFamily: "72", fontSize: 18, color: "#515559"  }]}>
-//           {props.firstName} {props.lastName}
-//         </span>
-//       </div>
-//       <div style={[{ paddingTop: 10,paddingBottom: 10, marginLeft: 70}]}>
-//         <span style={[{ fontFamily: "72-bold", fontSize: 18, color: "#515559" }]}>Email:</span>
-//         <span style={[{ fontFamily: "72", fontSize: 15, color: "#515559" } ]}>{props.email}</span>
-//       </div>
-//     </div>
-//   );
-// };
+const DetailsContainer = styled.div`
+  margin-top: 30px;
+  margin-left: ${rowHeight - 10}px;
+`;
+const Name = styled.div`
+  font-size: 16px;
+  font-family: "72";
+  color: #515559;
+`;
+
+const DetailsKey = styled.div`
+  font-size: 16px;
+  font-family: "72";
+  color: #515559;
+  font-weight: 600;
+`;
+
+const DetailsValue = styled.div`
+  font-size: 14px;
+  font-family: "72";
+  color: #6f7275;
+  font-weight: 100;
+`;
+
+const ContactCard = props => {
+  return (
+    <RowContainer>
+      <CardHeader>
+        <LogoContainer id={props.id}>
+          <Logo>
+            {props.firstName.charAt(0)}
+            {props.lastName.charAt(0)}
+          </Logo>
+        </LogoContainer>
+        <Name>
+          {props.firstName} {props.lastName}
+        </Name>
+      </CardHeader>
+      <DetailsContainer>
+        <DetailsKey>Email:</DetailsKey>
+        <DetailsValue>{props.email}</DetailsValue>
+      </DetailsContainer>
+    </RowContainer>
+  );
+};
 class MainPage extends React.Component {
-  // state = {
-  //   fontLoaded: false
-  // };
 
-  async componentDidMount() {
-    // await Font.loadAsync({
-    //   "72": require("./../../../assets/fonts/72regular.ttf"),
-    //   "72-bold": require("./../../../assets/fonts/72bold.ttf")
-    // });
-    // this.setState({ fontLoaded: true });
-  }
+
   render() {
-    // if (!this.props.contacts.loading) {
-    //   if (this.props.contacts.contacts) {
-    //     contacts = this.props.contacts.contacts;
-    //     contactsLength = contacts.length;
-    //   }
-    // }
-    contacts = [
-      {
-        id: 1,
-        email: "Michele_Stokes@gmail.com",
-        firstName: "Wosianne",
-        lastName: "Wreenholt"
-      },
-      {
-        id: 2,
-        email: "Denis_Ryan32@hotmail.com",
-        firstName: "Moses",
-        lastName: "Koelpin"
-      },
-      {
-        id: 3,
-        email: "Josefa_Kiehn@yahoo.com",
-        firstName: "Kaylee",
-        lastName: "Casper"
-      },
-      {
-        id: 4,
-        email: "Golden90@hotmail.com",
-        firstName: "Mack",
-        lastName: "Kessler"
-      },
-      {
-        id: 5,
-        email: "Joanny_Kub@gmail.com",
-        firstName: "Adah",
-        lastName: "Ernser"
-      },
-      {
-        id: 6,
-        email: "Gabrielle.Rippin@yahoo.com",
-        firstName: "Clyde",
-        lastName: "Heaney"
-      },
-      {
-        id: 7,
-        email: "Luella_Beer@hotmail.com",
-        firstName: "Eda",
-        lastName: "Dibbert"
-      },
-      {
-        id: 8,
-        email: "Lucas_Harris@gmail.com",
-        firstName: "Eduardo",
-        lastName: "Corkery"
-      },
-      {
-        id: 9,
-        email: "Michele_Stokes@gmail.com",
-        firstName: "Josianne",
-        lastName: "Greenholt"
-      },
-      {
-        id: 10,
-        email: "Denis_Ryan32@hotmail.com",
-        firstName: "Moses",
-        lastName: "Koelpin"
-      },
-      {
-        id: 11,
-        email: "Michele_Stokes@gmail.com",
-        firstName: "Josianne",
-        lastName: "Greenholt"
-      },
-      {
-        id: 12,
-        email: "Denis_Ryan32@hotmail.com",
-        firstName: "Moses",
-        lastName: "Koelpin"
-      },
-      {
-        id: 13,
-        email: "Josefa_Kiehn@yahoo.com",
-        firstName: "Kaylee",
-        lastName: "Casper"
-      },
-      {
-        id: 14,
-        email: "Golden90@hotmail.com",
-        firstName: "Mack",
-        lastName: "Kessler"
-      },
-      {
-        id: 15,
-        email: "Joanny_Kub@gmail.com",
-        firstName: "Adah",
-        lastName: "Ernser"
-      },
-      {
-        id: 16,
-        email: "Gabrielle.Rippin@yahoo.com",
-        firstName: "Clyde",
-        lastName: "Heaney"
-      },
-      {
-        id: 17,
-        email: "Luella_Beer@hotmail.com",
-        firstName: "Eda",
-        lastName: "Dibbert"
-      },
-      {
-        id: 18,
-        email: "Lucas_Harris@gmail.com",
-        firstName: "Eduardo",
-        lastName: "Corkery"
-      },
-      {
-        id: 19,
-        email: "Michele_Stokes@gmail.com",
-        firstName: "Josianne",
-        lastName: "Greenholt"
-      },
-      {
-        id: 20,
-        email: "Denis_Ryan32@hotmail.com",
-        firstName: "Moses",
-        lastName: "Koelpin"
+    if (!this.props.contacts.loading) {
+      if (this.props.contacts.contacts) {
+        contacts = this.props.contacts.contacts;
+        contactsLength = contacts.length;
       }
-    ];
-    contactsLength = contacts.length;
-    console.log('contactsLength',contacts,'contacts',contacts);
+    }
+    // contacts = [
+    //   {
+    //     id: 1,
+    //     email: "Michele_Stokes@gmail.com",
+    //     firstName: "Wosianne",
+    //     lastName: "Wreenholt"
+    //   },
+    //   {
+    //     id: 2,
+    //     email: "Denis_Ryan32@hotmail.com",
+    //     firstName: "Moses",
+    //     lastName: "Koelpin"
+    //   },
+    //   {
+    //     id: 3,
+    //     email: "Josefa_Kiehn@yahoo.com",
+    //     firstName: "Kaylee",
+    //     lastName: "Casper"
+    //   },
+    //   {
+    //     id: 4,
+    //     email: "Golden90@hotmail.com",
+    //     firstName: "Mack",
+    //     lastName: "Kessler"
+    //   },
+    //   {
+    //     id: 5,
+    //     email: "Joanny_Kub@gmail.com",
+    //     firstName: "Adah",
+    //     lastName: "Ernser"
+    //   },
+    //   {
+    //     id: 6,
+    //     email: "Gabrielle.Rippin@yahoo.com",
+    //     firstName: "Clyde",
+    //     lastName: "Heaney"
+    //   },
+    //   {
+    //     id: 7,
+    //     email: "Luella_Beer@hotmail.com",
+    //     firstName: "Eda",
+    //     lastName: "Dibbert"
+    //   },
+    //   {
+    //     id: 8,
+    //     email: "Lucas_Harris@gmail.com",
+    //     firstName: "Eduardo",
+    //     lastName: "Corkery"
+    //   },
+    //   {
+    //     id: 9,
+    //     email: "Michele_Stokes@gmail.com",
+    //     firstName: "Josianne",
+    //     lastName: "Greenholt"
+    //   },
+    //   {
+    //     id: 10,
+    //     email: "Denis_Ryan32@hotmail.com",
+    //     firstName: "Moses",
+    //     lastName: "Koelpin"
+    //   },
+    //   {
+    //     id: 11,
+    //     email: "Michele_Stokes@gmail.com",
+    //     firstName: "Josianne",
+    //     lastName: "Greenholt"
+    //   },
+    //   {
+    //     id: 12,
+    //     email: "Denis_Ryan32@hotmail.com",
+    //     firstName: "Moses",
+    //     lastName: "Koelpin"
+    //   },
+    //   {
+    //     id: 13,
+    //     email: "Josefa_Kiehn@yahoo.com",
+    //     firstName: "Kaylee",
+    //     lastName: "Casper"
+    //   },
+    //   {
+    //     id: 14,
+    //     email: "Golden90@hotmail.com",
+    //     firstName: "Mack",
+    //     lastName: "Kessler"
+    //   },
+    //   {
+    //     id: 15,
+    //     email: "Joanny_Kub@gmail.com",
+    //     firstName: "Adah",
+    //     lastName: "Ernser"
+    //   },
+    //   {
+    //     id: 16,
+    //     email: "Gabrielle.Rippin@yahoo.com",
+    //     firstName: "Clyde",
+    //     lastName: "Heaney"
+    //   },
+    //   {
+    //     id: 17,
+    //     email: "Luella_Beer@hotmail.com",
+    //     firstName: "Eda",
+    //     lastName: "Dibbert"
+    //   },
+    //   {
+    //     id: 18,
+    //     email: "Lucas_Harris@gmail.com",
+    //     firstName: "Eduardo",
+    //     lastName: "Corkery"
+    //   },
+    //   {
+    //     id: 19,
+    //     email: "Michele_Stokes@gmail.com",
+    //     firstName: "Josianne",
+    //     lastName: "Greenholt"
+    //   },
+    //   {
+    //     id: 20,
+    //     email: "Denis_Ryan32@hotmail.com",
+    //     firstName: "Moses",
+    //     lastName: "Koelpin"
+    //   }
+    // ];
+    // contactsLength = contacts.length;
     return (
       <Center>
-              {contacts &&
-                contactsLength > 2 && (
-                  <CardStack1>
-
-<CardStack
-	height={55*contactsLength}
-	width={400}
-	background='#f8f8f8'
-	hoverOffset={25}>
-                  {contacts.map((contact, index) => (
-                    
-                    
-                    
-                    <Card key={index} background={colors[index%colors.length]}>
-                      <Title>{contact.firstName} {contact.lastName}</Title>
-                    </Card>
-
-
-))}
-        
-</CardStack>
-</CardStack1>
-                )}
-            
+        <CardStack1>
+          <TitleContainer>
+            <Title>Contacts List</Title>
+          </TitleContainer>
+          {contacts &&
+            contactsLength > 2 && (
+              <CardStack
+                height={rowHeight * contactsLength}
+                width={400}
+                background="#f8f8f8"
+                hoverOffset={20}
+              >
+                {contacts.map((contact, index) => (
+                  <Card key={index} background="#ffffff">
+                    <ContactCard {...contact} contactsLength={contactsLength} />
+                  </Card>
+                ))}
+              </CardStack>
+            )}
+        </CardStack1>
       </Center>
     );
   }
