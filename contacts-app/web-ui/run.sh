4733d352c6efe678143ed6568fbb97cd6c1d6613
Pavel Pavlov <pavel.pavlov@sap.com>
2018-07-18 11:41:59 +0200

    workarounding ko runtime issue

--- contacts-app/web-ui/run.sh
+++ contacts-app/web-ui/run.sh
@@ -7,4 +7,9 @@ else
  sed -i "s#http://localhost/graphql#${BACKEND_URL}#" /etc/nginx/nginx.conf
 fi
 
+# Check if /var/log/nginx/ is created. Workaroud for ko builds...
+if [ ! -d "/var/log/nginx/logs" ]; then
+  echo "[INFO] /var/log/nginx/logs missing. running creating it."
+  mkdir -p /var/log/nginx/logs
+fi
 exec nginx -g "daemon off;"
